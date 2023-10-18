d875098437df0e5b52de55f798a5b2cc028033bf
unknown <kamatabhishek14@gmail.com>
2023-10-18 18:23:17 +0530

    added navbar component using reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import { Button } from 'reactstrap'
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
 
 
   return(
     <>
+     <Header light={true} expand="md" container="md" color="light" fixed="top"/>
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
+// css import
+import "./header.css"
+
+function Header(args) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div >
+      <Navbar {...args}>
+        <NavbarBrand id='title' href="/">Shopcart</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ms-auto" navbar>
+            <UncontrolledDropdown nav inNavbar style={{marginRight:'2rem'}}>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Username</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;

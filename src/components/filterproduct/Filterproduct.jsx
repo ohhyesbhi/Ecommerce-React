d09fab67da25a4f4edd3be0560a9a803c07a0475
unknown <kamatabhishek14@gmail.com>
2023-10-19 11:27:20 +0530

    added filter product to product list

---
+++ src/components/filterproduct/Filterproduct.jsx
@@ -0,0 +1,64 @@
+import "./filterproduct.css"
+
+function Filterproduct() {
+
+    const minPrice = [0,10,20,50,100,200];
+    const maxPrice = [0,10,20,50,100,200,1000];
+
+  return (
+   <>
+   <div className="product-list-sidebar d-flex flex-column">
+        <div className="sidebar-title">Search Products</div>
+        <div className="sidebar-search form-group">
+          <input
+            type="text"
+            placeholder="Search by name"
+            className="form-control"
+            id="searchInput"
+          />
+        </div>
+
+        <div className="sidebar-title fw-bold">Categories</div>
+        <div id="categoryList">
+         <a className='d-flex text-decoration-none '>Electronics</a>
+         <a className='d-flex text-decoration-none '>Jewelery</a>
+         <a className='d-flex text-decoration-none '>Men clothing</a>
+         <a className='d-flex text-decoration-none '>Women clothing </a>
+
+        </div>
+
+        <div className="sidebar-title">Filter by price</div>
+        <div className="price-filter">
+          <div className="price-filter-select d-flex flex-row justify-content-between">
+            <div className="form-group">
+              <select name="minPrice" className="form-select" id="minPrice">
+                {
+                  minPrice.map((price)=><option key={price} value={price}>{price}</option>)
+                }
+              </select>
+            </div>
+            <div className="form-group">
+              <select name="maxPrice" className="form-select" id="maxPrice">
+              {
+                  maxPrice.map((price)=><option key={price} value={price}>{price}</option>)
+                }
+              </select>
+            </div>
+          </div>
+          <div className="price-filter-title d-flex flex-row justify-content-around">
+            <div id="price-filter-label-min">Min Price</div>
+            <div id="price-filter-label-max">Max Price</div>
+          </div>
+        </div>
+        <button className="btn btn-warning clear-filter" id="search">
+          Search
+        </button>
+        <button className="btn btn-danger clear-filter" id="clear">
+          Clear Filters
+        </button>
+      </div>
+   </>
+  )
+}
+
+export default Filterproduct
--- src/pages/productlist/ProductList.jsx
+++ src/pages/productlist/ProductList.jsx
@@ -2,14 +2,16 @@ import Productbox from "../../components/productbox/Productbox"
 import "./productlist.css"
 
 import Image from "../../assets/wepik-export-20230815191001RxUw.png"
+import Filterproduct from "../../components/filterproduct/Filterproduct"
 
 function ProductList() {
   return (
 <>
- <div className="container">
+ <div className="container" style={{marginTop:"1rem"}}>
     <div className="row">
     <h2 className="product-list-title text-center">All products</h2>
     <div className="product-list-wrapper d-flex flex-row">
+      <Filterproduct/>
        {/**list of products */}
        <div className="product-list-box" id="productList">
           <Productbox productImage={Image} name={"dummy"} price={1000}/>
